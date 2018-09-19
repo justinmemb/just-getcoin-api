267484203e09c58b7f46e95ed50f386357840e87
David Bucci <>
2018-09-19 08:14:00 -0300

    completed chat

--- api/controllers/chats.js
+++ api/controllers/chats.js
@@ -539,16 +539,430 @@ if (error) {
                                 }).sort({created_at:1});
                             }
                         }).sort({created_at:1});
+                    } else {
+
+                        // Sender not exists
+                        response.json({
+
+                            error: true,
+                            message: message.invalidSender
+                        });
                     }
-                
-                
-            }
-           
-        });
-       
+                }
+            });
+        } else {
+
+            // Sender not exists
+            response.json({
+
+                error: true,
+                message: message.invalidSender
+            });
+        }
     }
+};
+ 
+                
+                
+
+exports.confirmMeetingAction = function(request, response) {
+
+    // Verify Required Parameters
+    var required_fields = ['to_user_id', 'for_user_id', 'offer_id', 'action'];
+
+    var sender_id = request.body.for_user_id;
+    var receiver_id = request.body.to_user_id;
+    var offer_id = request.body.offer_id;
+    var action = request.body.action;
+
+    var error = false;
+    var error_fields = "";
+
+    var request_params = JSON.stringify(request.body);
+    var objectValue = JSON.parse(request_params);
+
+    required_fields.forEach(function(element) {
+
+        if (!objectValue[element]) {
+
+            error = true;
+            error_fields += element + ', ';
+        }
+    });
+
+    if (error) {
+
+        // Required field(s) are missing or empty
+        response.json({
+
+            "error": true,
+            "message": 'Required field(s) ' + error_fields + 'is missing or empty!'
+        });
+    } else {
+
+        // Sender
+        if (object_id.isValid(sender_id)) {
+
+            users.find({_id:sender_id}, function(error, for_user) {
+
+                if (error) {
+
+                    response.json({
+
+                        error: true,
+                        error_description: error.message,
+                        message: message.serverErrorOccurred
+                    });
+                } else {
+
+                    if (for_user.length) {
+
+                        // Sender exists
+                        // Receiver
+                        if (object_id.isValid(receiver_id)) {
+
+                            users.find({_id:receiver_id}, function(error, to_user) {
+
+                                if (error) {
+
+                                    response.json({
+
+                                        error: true,
+                                        error_description: error.message,
+                                        message: message.serverErrorOccurred
+                                    });
+                                } else {
+
+                                    if (to_user.length) {
+
+                                        // Receiver exists
+                                        // Offer
+                                        if (object_id.isValid(offer_id)) {
+
+                                            offers.find({_id:offer_id}, function(error, for_offer) {
+
+                                                if (error) {
+
+                                                    response.json({
+
+                                                        error: true,
+                                                        error_description: error.message,
+                                                        error: message.serverErrorOccurred
+                                                    });
+                                                } else {
+
+                                                    if (for_offer.length) {
+
+                                                        // Offer exists
+                                                        var offer_params = JSON.stringify(for_offer[0]);
+                                                        var offer_details = JSON.parse(offer_params);
+
+                                                        var offer_creator = offer_details['offer_by'];
+
+                                                        if (offer_creator != sender_id) {
+
+                                                            if (action ===  '1') {
+
+                                                                // Offer Accept
+                                                                chats_controller.createDeal(sender_id, receiver_id, offer_id, 'a', function (result) {
+
+                                                                    response.json(result);
+                                                                });
+                                                            } else if (action ===  '0') {
+
+                                                                // Offer Reject
+                                                                chats_controller.createDeal(sender_id, receiver_id, offer_id, 'r', function (result) {
+
+                                                                    response.json(result);
+                                                                });
+                                                            } else {
+
+                                                                // Invalid Action
+                                                                response.json({
+
+                                                                    error: true,
+                                                                    message: message.invalidAction
+                                                                });
+                                                            }
+                                                        } else {
+
+                                                            // Can't take action your own offer
+                                                            response.json({
+
+                                                                error: true,
+                                                                message: message.offerActionNotApplicable
+                                                            });
+                                                        }
+                                                    } else {
+
+                                                        // Offer not exists
+                                                        response.json({
+
+                                                            error: true,
+                                                            message: message.invalidOffer
+                                                        });
+                                                    }
+                                                }
+                                            });
+                                        } else {
+
+                                            // Offer not exists
+                                            response.json({
+
+                                                error: true,
+                                                message: message.invalidOffer
+                                            });
+                                        }
+                                    } else {
+
+                                        // Receiver not exists
+                                        response.json({
+
+                                            error: true,
+                                            message: message.invalidReceiver
+                                        });
+                                    }
+                                }
+                            });
+                        } else {
+
+                            // Receiver not exists
+                            response.json({
+
+                                error: true,
+                                message: message.invalidReceiver
+                            });
+                        }
+
+                    } else {
+
+                        // Sender not exists
+                        response.json({
+
+                            error: true,
+                            message: message.invalidSender
+                        });
+                    }
+                }
+            });
+        } else {
+
+            // Sender not exists
+            response.json({
+
+                error: true,
+                message: message.invalidSender
+            });
+        }
+    }
+};
 
 
-}
+exports.getToken = function(user_id, callback) {
+
+    firebase_admin.auth().createCustomToken(user_id) .then(function(token) {
+
+        return callback(null, token);
+    }) .catch(function(error) {
+
+        return callback(error, null);
+    });
+
+};
+
+
+exports.sendReceiverMessage = function(cloudkey, payload, options, callback) {
+
+    if (cloudkey) {
+
+        firebase_admin.messaging().sendToDevice(cloudkey, payload, options).then(function(response) {
+
+            return callback(null, response);
+        }) .catch(function(error) {
+
+            return callback(error, null);
+        });
+    }
+};
+
+
+
+exports.createDeal = function (sender, receiver, offer, action, callback) {
+
+    var result;
+    deals.findOne({ for_user: sender, to_user: receiver, offer_id: offer }, function(error, deal) {
+
+        if (!error && deal) {
+
+            // Deal exists
+            result ={
+
+                error: true,
+                message : message.offerActionAlready
+            };
+            return callback(result);
+        } else {
+
+            // Deal not exists
+            var status = 'p';
+            if (action === 'r') {
+
+                status = 'd';
+            }
+
+            var deal = {
+
+                for_user: receiver,
+                to_user: sender,
+                offer_id: offer,
+                offer_status: action,
+                deal_status: status
+            };
+
+            var new_deal = new deals(deal);
+            new_deal.save(function(error, result_deal) {
+
+                if (error) {
+
+                    result = {
+
+                        error: true,
+                        error_description: error.message,
+                        error: message.serverErrorOccurred
+                    };
+
+                    return callback(result);
+                } else {
+
+                    var action_message;
+                    if (action === 'a') {
+
+                        action_message = message.offerAccepted;
+                        users_controller.getUserById(sender, function (error, result_user) {
+
+                            if (result_user) {
+
+                                var user_params = JSON.stringify(result_user);
+                                var user_details = JSON.parse(user_params);
+                                var notification = 'You have accept offer from '+ user_details.name;
+
+                                notifications_controller.createNotifications(receiver, sender, notification, offer, function(error, result_notification) {
+
+                                    if (result_notification) {
+
+                                        console.log(message.notificationGenerated);
+                                    }
+                                });
+                            }
+                        });
+
+
+                        users_controller.getUserById(receiver, function (error, result_user) {
+
+                            if (result_user) {
+
+                                var user_params = JSON.stringify(result_user);
+                                var user_details = JSON.parse(user_params);
+                                var notification = 'Your offer is accepted by '+ user_details.name;
+
+                                notifications_controller.createNotifications(sender, receiver, notification, offer, function(error, result_notification) {
+
+                                    if (result_notification) {
+
+                                        console.log(message.notificationGenerated);
+                                    }
+                                });
+                            }
+                        });
+                    }  else if (action === 'r') {
+
+                        action_message = message.offerRejected;
+                        users_controller.getUserById(sender, function (error, result_user) {
+
+                            if (result_user) {
+
+                                var user_params = JSON.stringify(result_user);
+                                var user_details = JSON.parse(user_params);
+                                var notification = 'You have reject offer from '+ user_details.name;
+
+                                notifications_controller.createNotifications(receiver, sender, notification, offer, function(error, result_notification) {
+
+                                    if (result_notification) {
+
+                                        console.log(message.notificationGenerated);
+                                    }
+                                });
+                            }
+                        });
+
+
+                        users_controller.getUserById(receiver, function (error, result_user) {
+
+                            if (result_user) {
+
+                                var user_params = JSON.stringify(result_user);
+                                var user_details = JSON.parse(user_params);
+                                var notification = 'Your offer is rejected by '+ user_details.name;
+
+                                notifications_controller.createNotifications(sender, receiver, notification, offer, function(error, result_notification) {
+
+                                    if (result_notification) {
+
+                                        console.log(message.notificationGenerated);
+                                    }
+                                });
+                            }
+                        });
+                    }
+
+
+                    result = {
+ 
+                        error: false,
+                        message: action_message
+                    };
+
+                    return callback(result);
+                }
+            });
+        }
+    });
+};
+
+exports.getChat = function (sender, receiver, callback) {
+
+    var result;
+    chats.find({$or: [ {for_user: sender, to_user: receiver}, {for_user: receiver, to_user:sender} ] }, function(error, chat_messgas) {
+
+        if (error) {
+
+            result = {
+
+                error: true,
+                error_description: error.message,
+                message: message.serverErrorOccurred
+            };
+            callback(result);
+        } else {
+
+            var array_chats = new Array();
+            for (var i = 0; i < chat_messgas.length; i++) {
+
+                array_chats.push(formatChats(chat_messgas[i]));
+            }
+
+            result = {
+
+                error: false,
+                chats: array_chats,
+                message: message.chatListed
+            };
+            callback(result);
+        }
+    }).sort({created_at:1});
+};
+
+
+
 
 
